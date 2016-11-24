fa556333be6f3715abfb7f7114cc8e879da8fb14
FNCxPro <FNCxPro@users.noreply.github.com>
2016-11-24 17:06:10 -0500

    Add ecosystem.config.js and update package.json

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+      name      : "Cyclone",
+      script    : "index.js",
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production : {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+  deploy : {
+    production : {
+      user : "node",
+      host : "chicago.atlanta.local",
+      ref  : "origin/master",
+      repo : "git@github.com:FNCxPro/cyclone.git",
+      path : "/home/node/cyclone/",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env production"
+    }
+  }
+}
