520499032bce6c2af6849a70991cd87c773fdfe9
FNCxPro <FNCxPro@users.noreply.github.com>
2016-11-25 15:29:51 -0500

    Error fixing

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use((req,res,next) => {
 
 app.use((err,req,res,next) => {
   res.locals.message = err.message
-  res.locals.status = err.status
+  res.locals.status = err.status || 500
   res.locals.error = config.production ? {} : err
   res.status(err.status || 500)
   res.render('error')
