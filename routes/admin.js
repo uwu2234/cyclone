176ada9a2f6989bccd3a6d1eeafeedc162427f6a
FNCxPro <FNCxPro@users.noreply.github.com>
2016-11-26 14:53:12 -0500

    admin

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@
  */
 const express = require('express')
 const requestify = require('requestify')
+const config = require('../config.json')
 const jwt = require('jsonwebtoken')
 const router = express.Router()
 
