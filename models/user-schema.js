87a275963359513168c12b5dd517c7096a96a8b8
FNCxPro <FNCxPro@users.noreply.github.com>
2016-11-26 23:30:06 -0500

    FUCK MYSELF IM STUPID

--- models/user-schema.js
+++ models/user-schema.js
@@ -5,8 +5,7 @@
  * Created on.........: 11/24/2016
  */
 
-const _mongoose = require('mongoose')
-const mongoose =  _mongoose.createConnection('admin:XpCdV6K1DWwq4BW0k0l@178.32.177.169/cyclone?authSource=admin&authMechanism=SCRAM-SHA-1')
+const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
   id: {
     type: String,
