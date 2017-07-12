4ca7419f7e5c2489caa6a83c98c60724ab7c63a9
FNCxPro <relative@outlook.ie>
2017-07-12 15:28:23 -0400

    prefix reset

--- commands/serverconfig.js
+++ commands/serverconfig.js
@@ -14,10 +14,26 @@ module.exports = function (bot, db, log) {
     purple: '#5E239D',
     green2: '#0DAB76'
   }
-  bot.registerCommand('prefix', (msg, args) => {
+  let prefixCommand = bot.registerCommand('prefix', (msg, args) => {
     bot.registerGuildPrefix(msg.channel.guild.id, ['@mention ', args[0] + ' '])
     db.r.table('servers').get(msg.channel.guild.id).update({prefix: args[0] + ' '}).run()
-    return `Prefix set to ${args[0]}`
+    return `Prefix set to \`${args[0]}\``
+  }, {
+    description: 'Set the prefix for the current guild (requires ADMINISTRATOR perm)',
+    fullDescription: 'Set the prefix for the current guild (requires ADMINISTRATOR perm)',
+    guildOnly: true,
+    argsRequired: true,
+    requirements: {
+      userIDs: ['116693403147698181'],
+      permissions: {
+        administrator: true
+      }
+    }
+  })
+  prefixCommand.registerSubCommand('reset', (msg, args) => {
+    db.r.table('servers').get(msg.channel.guild.id).update({prefix: undefined}).run()
+    bot.registerGuildPrefix(msg.channel.guild.id, ['cy!', '@mention '])
+    return `Prefix reset to \`cy!\``
   }, {
     description: 'Set the prefix for the current guild (requires ADMINISTRATOR perm)',
     fullDescription: 'Set the prefix for the current guild (requires ADMINISTRATOR perm)',
