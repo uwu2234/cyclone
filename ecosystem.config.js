bb535414442d7cbc47f69b20fde01d7f4aa415ee
FNCxPro <relative@outlook.ie>
2017-07-12 14:45:34 -0400

    Ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name: 'Cyclone',
+      script: 'index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'cyclone',
+      host : '69.195.152.138',
+      ref  : 'origin/master',
+      repo : 'git@github.com:FNCxPro/cyclone.git',
+      path : '/home/cyclone/prod',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev: {
+      user : 'cyclone',
+      host : '192.168.7.5',
+      ref  : 'origin/master',
+      repo : 'git@github.com:FNCxPro/cyclone.git',
+      path : '/home/cyclone/dev',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
