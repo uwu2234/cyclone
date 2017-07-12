e314c22a958ea7da046368943ad1cb74b2aad371
FNCxPro <relative@outlook.ie>
2017-07-12 15:08:19 -0400

    ecosytem changes prod

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -3,5 +3,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 npm install
-pm2 reload ecosystem.config.js --env production
+#pm2 reload ecosystem.config.js --env production
 pm2 startOrRestart ecosystem.config.js --env production
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,38 +1,35 @@
 module.exports = {
-
-  apps : [
-    {
-      name: 'Cyclone',
-      script: 'index.js',
-      env: {
-        COMMON_VARIABLE: 'true'
-      },
-      env_production : {
-        NODE_ENV: 'production'
-      }
+  apps: [{
+    name: 'Cyclone',
+    script: 'index.js',
+    env_production : {
+      NODE_ENV: 'production'
+    },
+    env_development: {
+      NODE_ENV: 'dev'
     }
-  ],
+  }],
   /**
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy : {
-    production : {
-      user : 'cyclone',
-      host : '69.195.152.138',
-      ref  : 'origin/master',
-      repo : 'git@github.com:FNCxPro/cyclone.git',
-      path : '~/cyclone',
-      'post-deploy' : 'chmod +x ~/cyclone/deploy/prod.sh && ./deploy/prod.sh'
+  deploy: {
+    production: {
+      user: 'cyclone',
+      host: '69.195.152.138',
+      ref: 'origin/master',
+      repo: 'git@github.com:FNCxPro/cyclone.git',
+      path: '~/cyclone',
+      'post-deploy': 'chmod +x ~/cyclone/deploy/prod.sh && ./deploy/prod.sh'
     },
-    dev: {
-      user : 'cyclone',
-      host : '192.168.7.5',
-      ref  : 'origin/master',
-      repo : 'git@github.com:FNCxPro/cyclone.git',
-      path : '/home/cyclone/dev',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
+    development: {
+      user: 'cyclone',
+      host: '192.168.7.5',
+      ref: 'origin/master',
+      repo: 'git@github.com:FNCxPro/cyclone.git',
+      path: '/home/cyclone/dev',
+      'post-deploy': 'chmod +x ~/cyclone/deploy/prod.sh && ./deploy/prod.sh',
+      env: {
         NODE_ENV: 'dev'
       }
     }
